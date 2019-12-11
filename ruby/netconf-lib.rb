b0c1520844f534499fe15a79151e88ac2a90b008
joelasir <joelcabreraberriel@gmail.com>
2019-12-11 10:27:23 +0000

    1

---
+++ ruby/netconf-lib.rb
@@ -0,0 +1,17 @@
+def menu
+  puts "Select an option: "
+  puts "1) reset"
+  puts "2) aula109.static"
+  puts "3) casa.static"
+  option = gets.chomp
+  if option == 1
+    system('sudo ifdown eth0')
+    system('sudo ifup eth0')
+  elsif option == 2
+    system('sudo ip addr add 172.19.8.1/16 dev eth0')
+  elsif option == 3
+    system('sudo ip addr add 192.168.1.1/24 dev eth0')
+  else
+    puts "Select a correct option"
+  end
+end
---
+++ ruby/netconf.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+require_relative 'netconf-lib'
+menu
